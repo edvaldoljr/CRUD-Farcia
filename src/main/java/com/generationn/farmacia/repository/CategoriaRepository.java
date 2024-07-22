615cbadd621f0f7ea16be1e3999a3050b4a676cb
edvaldoljr <edvaldoluizj@gmail.com>
2024-07-22 11:38:25 -0300

    CRUD Categoria

--- src/main/java/com/generationn/farmacia/repository/CategoriaRepository.java
+++ src/main/java/com/generationn/farmacia/repository/CategoriaRepository.java
@@ -1,4 +1,14 @@
 package com.generationn.farmacia.repository;
 
-public interface CategoriaRepository {
+import com.generationn.farmacia.model.CategoriaModel;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface CategoriaRepository extends JpaRepository<CategoriaModel, Long> {
+
+    List<CategoriaModel> findAllByNomeContainingIgnoreCase(@Param("tipo") String nome);
 }
